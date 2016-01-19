76f24d0e26a39dc12daeb02e44f9f5bfcfa3b136
Nicolas Fantone <nfantone@contentamobile.com>
2016-01-19 15:23:33 -0300

    Add support for pin attribute
    Create multiple queues based on pins
    Bind queues to seneca.direct using different rk

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var _ = require('lodash');
 var async = require('async');
 var amqp = require('amqplib/callback_api');
@@ -8,7 +10,7 @@ var defaults = {
         type: 'amqp',
         url: 'amqp://localhost',
         exchange: {
-            name: 'seneca',
+            name: 'seneca.direct',
             options: {
                 durable: true,
                 autoDelete: false
@@ -33,15 +35,17 @@ var hookListen = function(options, args, done) {
     var tu = seneca.export('transport/utils');
     async.auto({
         conn: function(cb) {
-            amqp.connect(listenOptions.url, cb);
+            return amqp.connect(listenOptions.url, cb);
         },
         channel: ['conn', function(cb, results) {
             var conn = results.conn;
             conn.createChannel(function(err, channel) {
-                if (err) { return cb(err); }
+                if (err) {
+                    return cb(err);
+                }
                 channel.prefetch(1);
                 channel.on('error', done);
-                cb(null, channel);
+                return cb(null, channel);
             });
         }],
         exchange: ['channel', function(cb, results) {
@@ -50,56 +54,89 @@ var hookListen = function(options, args, done) {
             var name = ex.name;
             var opts = ex.options;
             return channel.assertExchange(name, 'direct', opts, function(err, ok) {
-                if (err) { return cb(err); }
+                if (err) {
+                    return cb(err);
+                }
                 return cb(null, ok.exchange);
             });
         }],
-        topic: function(cb) {
-            tu.listen_topics(seneca, args, listenOptions, function(topic) {
-                cb(null, topic);
+        topics: function(cb) {
+            var pins = tu.resolve_pins(listenOptions);
+
+            var topics = [];
+
+            pins.forEach(function(p) {
+                topics.push('seneca.' + _.map(p, function(v, k) {
+                    return [k, v].join('.');
+                }).join('.'));
             });
-        },
-        actQueue: ['exchange', 'channel', 'topic', function(cb, results) {
-            var exchange = results.exchange;
-            var channel = results.channel;
-            var topic = results.topic;
-            var actQueue = topic + '_act';
-            var opts = listenOptions.queues.action;
-            channel.assertQueue(actQueue, opts, function(err, ok) {
-                if (err) { return cb(err); }
-                channel.bindQueue(actQueue, exchange, 'action', {}, function(err, ok) {
-                    if (err) { return cb(err); }
-                    return cb(null, actQueue);
+
+            topics = _.reject(topics, function(o) {
+                return _.find(topics, function(r) {
+                    return r.length < o.length && o.startsWith(r);
                 });
             });
+
+            return cb(null, topics);
+        },
+        actQueues: ['exchange', 'channel', 'topics', function(cb, results) {
+            var exchange = results.exchange;
+            var channel = results.channel;
+            var opts = listenOptions.queues.action;
+            var queues = [];
+
+            async.each(results.topics, function(topic, done) {
+                var actQueue = topic.replace(/\./g, '_') + '_act';
+                channel.assertQueue(actQueue, opts, function(err) {
+                    if (err) {
+                        return cb(err);
+                    }
+                    channel.bindQueue(actQueue, exchange, topic, {}, function(err) {
+                        if (err) {
+                            return cb(err);
+                        }
+                        queues.push(actQueue);
+                        return done();
+                    });
+                });
+            }, function(err) {
+                return cb(err, queues);
+            });
+
         }]
     }, function(err, results) {
-        if (err) { return done(err); }
+        if (err) {
+            return done(err);
+        }
         var conn = results.conn;
         var channel = results.channel;
         var exchange = results.exchange;
-        var actQueue = results.actQueue;
-        seneca.log.debug('listen', 'subscribe', actQueue, listenOptions, seneca);
-        channel.consume(actQueue, function(message) {
-            var tu = seneca.export('transport/utils');
-            var content = message.content ? message.content.toString() : void 0;
-            var props = message.properties || {};
-            var replyTo = props.replyTo;
+        var actQueues = results.actQueues;
+        seneca.log.debug('listen', 'subscribe', actQueues, listenOptions, seneca);
 
-            if (!content || !props.replyTo) {
-                return channel.nack(message);
-            }
+        _.each(actQueues, function(q) {
+            channel.consume(q, function(message) {
+                debugger;
+                var tu = seneca.export('transport/utils');
+                var content = message.content ? message.content.toString() : void 0;
+                var props = message.properties || {};
+                var replyTo = props.replyTo;
 
-            var data = tu.parseJSON(seneca, 'listen-' + type, content);
-
-            tu.handle_request(seneca, data, listenOptions, function(out) {
-                if( typeof out === 'undefined' || out === null ) {
-                    return;
+                if (!content || !props.replyTo) {
+                    return channel.nack(message);
                 }
-                var outstr = tu.stringifyJSON(seneca, 'listen-' + type, out);
 
-                channel.ack(message);
-                channel.sendToQueue(replyTo, new Buffer(outstr));
+                var data = tu.parseJSON(seneca, 'listen-' + type, content);
+
+                tu.handle_request(seneca, data, listenOptions, function(out) {
+                    if (typeof out === 'undefined' || out === null) {
+                        return;
+                    }
+                    var outstr = tu.stringifyJSON(seneca, 'listen-' + type, out);
+                    debugger;
+                    channel.ack(message);
+                    channel.sendToQueue(replyTo, new Buffer(outstr));
+                });
             });
         });
 
@@ -112,7 +149,7 @@ var hookListen = function(options, args, done) {
 
         seneca.log.info('listen', 'open', listenOptions, seneca);
 
-        done();
+        return done();
     });
 
 };
@@ -124,11 +161,11 @@ var hookClient = function(options, args, done) {
     var tu = seneca.export('transport/utils');
     async.auto({
         conn: function(cb) {
-            amqp.connect(clientOptions.url, cb);
+            return amqp.connect(clientOptions.url, cb);
         },
         channel: ['conn', function(cb, results) {
             var conn = results.conn;
-            conn.createChannel(cb);
+            return conn.createChannel(cb);
         }],
         exchange: ['channel', function(cb, results) {
             var channel = results.channel;
@@ -136,21 +173,22 @@ var hookClient = function(options, args, done) {
             var name = ex.name;
             var opts = ex.options;
             return channel.assertExchange(name, 'direct', opts, function(err, ok) {
-                if (err) { return cb(err); }
-                cb(null, ok.exchange);
+                return cb(err, ok.exchange);
             });
         }],
         resQueue: ['channel', function(cb, results) {
             var channel = results.channel;
-            var queueName = 'res_' + shortid.generate();
+            var queueName = 'seneca_res_' + shortid.generate();
             var opts = clientOptions.queues.response;
-            channel.assertQueue(queueName, opts, function(err, ok) {
-                if (err) { return cb(err); }
-                return cb(null, ok.queue);
+            return channel.assertQueue(queueName, opts, function(err, ok) {
+                return cb(err, ok.queue);
             });
         }]
     }, function(err, results) {
-        if (err) { return done(err); }
+        debugger;
+        if (err) {
+            return done(err);
+        }
         var conn = results.conn;
         var channel = results.channel;
         var exchange = results.exchange;
@@ -158,24 +196,26 @@ var hookClient = function(options, args, done) {
 
         channel.prefetch(1);
 
-        tu.make_client(function(spec, topic, sendDone) {
-
+        tu.make_client(seneca, function(spec, topic, sendDone) {
             channel.on('error', sendDone);
 
             seneca.log.debug('client', 'subscribe', resQueue, clientOptions, seneca);
-            channel.consume(resQueue, function (message) {
+            channel.consume(resQueue, function(message) {
                 var content = message.content ? message.content.toString() : undefined;
                 var input = tu.parseJSON(seneca, 'client-' + type, content);
                 tu.handle_response(seneca, input, clientOptions);
-            }, {noAck: true});
+            }, {
+                noAck: true
+            });
 
-            sendDone(null, function (args, done) {
+            sendDone(null, function(args, done) {
                 var outmsg = tu.prepare_request(this, args, done);
                 var outstr = tu.stringifyJSON(seneca, 'client-' + type, outmsg);
                 var opts = {
                     replyTo: resQueue
                 };
-                channel.publish(exchange, 'action', new Buffer(outstr), opts);
+                topic = 'seneca.' + args.meta$.pattern.replace(/:/g, '.');
+                channel.publish(exchange, topic, new Buffer(outstr), opts);
             });
 
             seneca.add('role:seneca,cmd:close', function(closeArgs, done) {
@@ -196,8 +236,18 @@ module.exports = function(opts) {
     var tu = seneca.export('transport/utils');
     var listen = hookListen.bind(seneca, options);
     var client = hookClient.bind(seneca, options);
-    seneca.add({role:'transport', hook:'listen', type:'amqp'}, listen);
-    seneca.add({role:'transport', hook:'client', type:'amqp'}, client);
+    seneca.add({
+        role: 'transport',
+        hook: 'listen',
+        type: 'amqp'
+    }, listen);
+    seneca.add({
+        role: 'transport',
+        hook: 'client',
+        type: 'amqp'
+    }, client);
 
-    return {name: plugin};
+    return {
+        name: plugin
+    };
 };
