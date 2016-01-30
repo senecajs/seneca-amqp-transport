58790519b164b4989f7fec3a86f76449718d4718
Nicolás Fantone <nfantone@gmail.com>
2016-01-30 19:29:59 -0300

    Setup seneca eslint config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var defaults = require('./defaults');
+var Defaults = require('./defaults');
 var ClientHook = require('./lib/client-hook');
 var ListenHook = require('./lib/listen-hook');
 
@@ -9,7 +9,7 @@ const PLUGIN_NAME = 'amqp-transport';
 module.exports = function(opts) {
   var seneca = this;
   var so = seneca.options();
-  var options = seneca.util.deepextend(defaults, so.transport, opts);
+  var options = seneca.util.deepextend(Defaults, so.transport, opts);
   var listen = new ListenHook(seneca, options);
   var client = new ClientHook(seneca, options);
   seneca.add({
