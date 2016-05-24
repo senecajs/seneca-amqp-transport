615c234f65a7fc1dcb67ff6ed0aec0f4f6ad29fe
Nicolás Fantone <nfantone@contentamobile.com>
2016-05-24 17:38:49 -0300

    Turn listener into ES6 classes

--- index.js
+++ index.js
@@ -7,22 +7,23 @@ const ClientHook = require('./lib/client-hook');
 const ListenHook = require('./lib/listen-hook');
 
 const PLUGIN_NAME = 'amqp-transport';
+const TRANSPORT_TYPE = 'amqp';
 
 module.exports = function(opts) {
   var seneca = this;
   var so = seneca.options();
   var options = seneca.util.deepextend(Defaults, so.transport, opts);
-  var listen = new ListenHook(seneca, options);
+  var listen = new ListenHook(seneca);
   var client = new ClientHook(seneca);
   seneca.add({
     role: 'transport',
     hook: 'listen',
-    type: 'amqp'
-  }, listen.hook());
+    type: TRANSPORT_TYPE
+  }, listen.hook(options));
   seneca.add({
     role: 'transport',
     hook: 'client',
-    type: 'amqp'
+    type: TRANSPORT_TYPE
   }, client.hook(options));
 
   return {
